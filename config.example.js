a670be3ecad362b4c77f6b886fe367d64f4ffd9e
Pallavi25Kishore <pallavi25kishore@gmail.com>
2024-07-08 17:59:10 -0700

    Initial repo setup

---
+++ config.example.js
@@ -0,0 +1,3 @@
+const API_KEY;
+const CAMPUS_CODE = "";
+const BASE_URL = "";
---
+++ server/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.use(express.json());
+
+
+app.listen(process.env.PORT);
+console.log(`Listening at http://localhost:${process.env.PORT}`);
